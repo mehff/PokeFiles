8b5dbdebb006802324d8b72ec2163d20f27a2638
mehff <mhfaciniferreira@gmail.com>
2022-04-13 15:51:04 -0300

    Added bootstrap, account creation, encryption

--- api.py
+++ api.py
@@ -1,13 +1,18 @@
 from hashlib import sha256
-from flask import Flask, Response, request, render_template
-# from flask_restful import Api, Resource
+from flask import Flask, Response, request, render_template, flash
+from wtforms import Form, TextAreaField, TextAreaField, validators
 import pymongo
 import json
-app = Flask(__name__)
 
-# Connection with local mongodb
+# App configuration
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config["SECRET_KEY"] = "f79c49f8cff36434256e56b610824ea695e88b36a23317c61cfdbd8b198b642c"
+
+# MongoDB configuration
 try:
-    # Mongo connection params
+    # Connection parameters
     mongo = pymongo.MongoClient(
         host="localhost",
         port=27017,
@@ -15,58 +20,74 @@ try:
     )
     db = mongo.GlobalStorageDB
     mongo.server_info()
-# Raise exception if cannot connect
+
+# Raise exception if connection fails
 except:
-    print("ERROR - Cannot connect to db")
+    print("ERROR: Connection to MongoDB failed")
+
+# WTForms registration
+class RegistrationForms(Form):
+    username = TextAreaField("Username:", validators=[validators.DataRequired()])
+    password = TextAreaField("Password:", validators=[validators.DataRequired(), validators.Length(min=6, max=35)])
+    email = TextAreaField("Email:", validators=[validators.DataRequired(), validators.Length(min=6, max=35)])
+
+# WTForms login
+class LoginForms(Form):
+    username = TextAreaField("Username:", validators=[validators.DataRequired()])
+    password = TextAreaField("Password:", validators=[validators.DataRequired(), validators.Length(min=6, max=35)])
+    email = TextAreaField("Email:", validators=[validators.DataRequired(), validators.Length(min=6, max=35)])
+
+# WTForms confirm email
+class ValidationForm(Form):
+    email = TextAreaField("Confirm your email:", validators=[validators.DataRequired(), validators.Length(min=6, max=35)])
+    validationCode = TextAreaField("Validation code:", validators=[validators.DataRequired()])
 
 # Routes
-@app.route("/users", methods=["POST"])
-@app.route("/")
-@app.route("/about")
+@app.route("/", methods=["GET", "POST"])
 
-def index():
-    return render_template("C:\\Users\\Matheus\\Documents\\GitHub\\GlobalStorage\\app\\templates\\public\\index.html")
+def regMain():
+    form = RegistrationForms(request.form)
+    print(form.errors)
+    
+    # Only accept POST requests
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        email = request.form["email"]
 
-def about():
-    return """
-    <h1 style='color: red;'>I'm a red H1 heading!</h1>
-    <p>This is a lovely little paragraph</p>
-    <code>Flask is <em>awesome</em></code>
-    """
-# Create a user on DB
-def create_user():
-    try:
-        encInfo = [request.form["userName"], request.form["userPw"], request.form["userPerms"]]
-        print(encInfo)
-        encryptedInfo = []
-        for i in encInfo:
-            print("BARIBERS",type(sha256(str(i).encode('utf8'))))
-            print(encryptedInfo.append(sha256(str(i).encode('utf-8')).hexdigest()))
-        encryptedInfo.append(request.form["userMail"])
-        print("BLABERS",encryptedInfo)
-        # Set user info
-        user = {
-            "userName":encryptedInfo[0],
-            "userPw":encryptedInfo[1],
-            "userPerms":encryptedInfo[2],
-            "userMail":encryptedInfo[3]
-        }
-        # Send to DB
-        dbResponse = db.users.insert_one(user)
-        print(dbResponse.inserted_id)
-        # # Print attrs
-        # for attr in dir(dbResponse):
-        #     print(attr)
-        return Response(
-            response=json.dumps(
-                {"message":"user created",
-                "id":f"{dbResponse.inserted_id}"
-                }),
-            status=200,
-            mimetype="application/json"
-        )
-    except Exception as ex:
-        print(ex)
+        # Validate data passed into forms
+        if form.validate():
+            
+            # Encrypt info and store into DB.
+            # @TODO Encrypt this directly.
+            info = [username, password, email]
+            encryptedInfo = []
+            for i in info:
+                encryptedInfo.append(sha256(str(i).encode('utf-8')).hexdigest())
 
+            # Visual indication of success
+            flash('Registration done. ', encryptedInfo)
+
+            # Compose dict with info
+            user = {
+                "username": encryptedInfo[0],
+                "password": encryptedInfo[1],
+                "email": encryptedInfo[2],
+                "perms": 0,
+                }
+            
+            # Send to DB
+            dbResponse = db.users.insert_one(user)
+            dbResponse.inserted_id
+            return render_template('/public/registration.html', form=form)
+
+        # Visual indication of failure
+        else:
+            flash('Error. All form fields are required. ')
+
+    # What to do if it fails
+    return render_template('/public/registration.html', form=form)
+
+# Run app
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
